2e546df4919dcbde70fdfe22816363228b13598b
nanzhu66 <xzxqq1@qq.com>
2019-06-17 19:58:52 +0800

    commit

--- src/main/java/com/xzkj/service/MyService.java
+++ src/main/java/com/xzkj/service/MyService.java
@@ -33,11 +33,8 @@ public class MyService {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String date = dateFormat.format(now);
         // 根据type和时间，将json数据保存到相应hdfs路径
-        String path = bundle.getString("path") + "ods_" + type + "_log/" + date + "/" + type + ".log";
+        String path = bundle.getString("path") + "/ods_" + type + "_log/" + date + "/" + type + ".log";
         hdfsDal.write(path, data);
-        /*TEST*/
-        System.out.println(redisDal);
-        System.out.println(hdfsDal);
         // 关闭资源
         redisDal.close();
         hdfsDal.close();
